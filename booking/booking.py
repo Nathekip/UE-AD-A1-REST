0c1b6e776319a78349e7ecc83c4370938640b011
Nathekip <nathaniel.guitton@etudiant.univ-rennes1.fr>
2024-09-28 11:20:23 +0200

    Ajout des méthodes /bookings et /bookings/<userid> dans booking.py

--- booking/booking.py
+++ booking/booking.py
@@ -15,6 +15,22 @@ with open('{}/databases/bookings.json'.format("."), "r") as jsf:
 def home():
    return "<h1 style='color:blue'>Welcome to the Booking service!</h1>"
 
+@app.route("/bookings/<userid>", methods=["POST"])
+def add_booking(userid):
+   req = request.get_json()
+   return
+
+@app.route("/bookings", methods=["GET"])
+def get_bookings():
+   return make_response(jsonify(bookings), 200)
+
+@app.route("/bookings/<userid>", methods=["GET"])
+def get_bookings_byuser(userid):
+   for booking in bookings:
+      print(booking)
+      if str(booking["userid"]) == str(userid):
+         return make_response(jsonify(booking), 200)
+   return make_response(jsonify({"error":"User ID not found"}),400)
 
 if __name__ == "__main__":
    print("Server running in port %s"%(PORT))
