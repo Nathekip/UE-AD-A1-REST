9f004e0438300d71c618e4efa2c0bc2de511f0fd
eloi.garnier <ext.eloi.garnier@reseau.sncf.fr>
2024-10-04 11:59:49 +0200

    ajout de '/users/movies/<user>' (non fonctionnel)

--- user/user.py
+++ user/user.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 
 PORT = 3203
 PORT_BOOKING = 3201
+PORT_MOVIE = 3200
 HOST = '0.0.0.0'
 IP = '127.0.0.1'
 
@@ -36,6 +37,25 @@ def get_bookings_byuser(user):
       return make_response({"error":"no bookings for this user"}, 400)
    return make_response(response.json())
 
+@app.route("/users/movies/<user>",methods=['GET'])
+def get_movies_byuser(user):
+   id = -1
+   for useri in users :
+      if useri["name"] == user or useri["id"] == user :
+         id = useri["id"]
+         break
+   if id == -1 :
+      return make_response(jsonify({"error":"user not found"}),400)
+   response = requests.get(f"http://{IP}:{PORT_BOOKING}/bookings/{id}")
+   if response.status_code != 200:
+      return make_response({"error":"no bookings for this user"}, 400)
+   movies_json = {"movies" : []}
+   for movies in response.json()["dates"] :
+      for movie in movies["movies"] :
+         movie_json = requests.get(f"http://{IP}:{PORT_MOVIE}/movies/{movie}")
+         print(response.json())
+         movies_json["movies"].append(movies_json)
+   return make_response(jsonify(movies_json))
 
 if __name__ == "__main__":
    print("Server running in port %s"%(PORT))
